947804e72288a88cd78e7bcd8b971fa0a63a8c8d
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-09-15 10:44:52 -0500

    commit tree 66d9924638d9abd3de86f9a2d51eb23f660caed5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+
+    def match(array) 
+
+        array.filter do |item| item.chars.sort == word.chars.sort
+           
+        end
+    end
+
+
+end
